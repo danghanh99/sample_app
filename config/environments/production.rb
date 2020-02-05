d7056c3936e41788f46723aa91501f91e498fa67
danghanh <ldhanh1999@gmail.com>
2020-02-05 17:57:54 +0700

    try to send active mail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,10 +66,10 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = '<your heroku app>.herokuapp.com'
+  host = 'whispering-caverns-20948.herokuapp.com'  
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
-    :address        => 'smtp.sendgrid.net',
+    :address        => 'smtp.gmail.com',
     :port           => '587',
     :authentication => :plain,
     :user_name      => ENV['MAIL_USERNAME'],
