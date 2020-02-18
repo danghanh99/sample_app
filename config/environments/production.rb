f78778a308d34aca26819d7b48c2385e959c43d5
danghanh <ldhanh1999@gmail.com>
2020-02-18 11:42:26 +0700

    sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,12 +70,12 @@ Rails.application.configure do
   host = 'sample-app-danghanh.herokuapp.com'  
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
-    :address        => 'smtp.gmail.com',
-    :port           => '587',
+    :address        => 'smtp.sendgrid.net',
+    :port           => '465',
     :authentication => :plain,
     :user_name      => ENV['MAIL_USERNAME'],
     :password       => ENV['MAIL_PASSWORD'],
-    :domain         => 'gmail.com',
+    :domain         => 'sendgrid.com',
     :enable_starttls_auto => true
   }
 
